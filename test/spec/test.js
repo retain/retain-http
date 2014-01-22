19d83d2a74ddd87279e948481025270f2066a7b6
Giulian Drimba <giuliandrimba@hotmail.com>
2014-01-21 23:51:03 -0200

    Setup tests

---
+++ test/spec/test.js
@@ -0,0 +1,11 @@
+var retainAjax = require("../../lib/index");
+var assert = require("chai").assert;
+
+describe("RetainAjax", function()
+{
+  it("should work", function(done)
+  {
+    assert.equal(true, true);
+    done();
+  })
+});
